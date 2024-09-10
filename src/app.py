ae3430938ff9053af03bcafb56119257dc50e73b
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-09-10 10:30:07 +0000

    primer commit

--- src/app.py
+++ src/app.py
@@ -25,18 +25,43 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+#OBTENER FAMILIA
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
-        "family": members
+        "miembors": members
     }
-
-
     return jsonify(response_body), 200
+
+#CREAR FAMILIAR
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    new_member = request.json
+    jackson_family.add_member(new_member)
+    return jsonify({"done" : "usuario creado"})
+
+#BORRAR FAMILIAR
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_family_member(member_id):
+    print(member_id)
+    return jsonify("probando")
+
+
+
+
+
+
+
+ 
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,14 +1,6 @@
-
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
 
-class FamilyStructure:
+class FamilyStructure:  
     def __init__(self, last_name):
         self.last_name = last_name
 
@@ -20,8 +12,10 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        if not member.get("id"):
+            member["id"] = self._generateId()
+        self._members.append(member)
+        
 
     def delete_member(self, id):
         # fill this method and update the return
